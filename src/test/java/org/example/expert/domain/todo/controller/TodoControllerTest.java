60d4df8e9007ccdcbc03af9b5b35555d7b43e74a
baek_do_hyeon <ehgus6666@naver.com>
2025-09-23 04:29:50 +0900

    lv2:9 Spring Security 테스트 코드 오류 해결

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -1,5 +1,7 @@
 package org.example.expert.domain.todo.controller;
 
+import org.example.expert.config.JwtSecurityProperties;
+import org.example.expert.config.JwtUtil;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -23,6 +26,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(TodoController.class)
 class TodoControllerTest {
+    @MockBean
+    JwtUtil jwtUtil;
+
+    @MockBean
+    JwtSecurityProperties jwtSecurityProperties;
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,6 +39,7 @@ class TodoControllerTest {
     private TodoService todoService;
 
     @Test
+    @WithMockUser
     void todo_단건_조회에_성공한다() throws Exception {
         // given
         long todoId = 1L;
